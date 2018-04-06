057e8b6af7ec8cddf9dbc0c09d78370a56e16fc9
Eileen <missingxf@yeah.net>
2018-04-06 11:46:42 +0800

    index by page

--- src/controllers/goods.js
+++ src/controllers/goods.js
@@ -14,12 +14,24 @@ let { User, Image, Goods } = require('../models');
 
 const router = module.exports = new Router();
 
-// 首页，显示最新的6个
+// 首页, 参数为pageNo(默认为1), pageSize(默认为6)
+// 返回值为 hasMore(有下一页), totle(记录总条数)
 router.get('/goods/index', async (ctx, next) => {
-    let goods = await Goods.find().sort('-_id').limit(6).populate('gpics');
+    let totle = await Goods.count();//表总记录数
+    let reqParam= ctx.query;
+    let pageNo = 1;
+    if (reqParam.pageNo) {pageNo = Number(reqParam.pageNo);}  //页码数, 默认为1
+    let pageSize = 6;
+    if (reqParam.pageSize) {pageSize = Number(reqParam.pageSize);}//每页显示的记录条数, 默认为6
+    console.log(pageNo);console.log(pageSize);
+    let goods = await Goods.find().sort('-_id').limit(pageSize).skip((pageNo-1)*pageSize).populate('gpics');
+    let hasMore=totle-pageNo*pageSize>0;
+    ctx.response.type = 'application/json';
     ctx.body = {
         success: 1,
-        data: await srv_goods.outputify(goods, ctx.state.user)
+        data: await srv_goods.outputify(goods, ctx.state.user),
+        hasMore:hasMore,
+        totle:totle
     }
 });
 
