e4b7c6f616e7921823255b5b9078f4e0dcfded14
Eileen <missingxf@yeah.net>
2018-04-15 09:45:47 +0800

    wechat post

--- src/controllers/wechat.js
+++ src/controllers/wechat.js
@@ -39,3 +39,37 @@ router.get('/wechat', async (ctx, next) => {
         ctx.body = { code: -1, msg: "fail"}
     }
 });
+
+
+/**
+ * @apiName 微信服务器事件监听窗口
+ */
+router.post('/wechat', async (ctx, next) => {
+    let params = ctx.query;
+    console.log(params);
+    let requestBody = ctx.request.body;
+    console.log(requestBody);
+    let xmlData = ctx.data.xml;
+    console.log(xmlData);
+
+    const token = config.SA_TOKEN, // 自定义，与公众号设置的一致
+        signature = params.signature,
+        timestamp = params.timestamp,
+        nonce = params.nonce;
+    auth.assert(signature, "MISS");
+    auth.assert(timestamp, "MISS");
+    auth.assert(nonce, "MISS");
+
+    // 字典排序
+    const arr = [token, timestamp, nonce].sort();
+    console.log(arr);
+    const sha1 = crypto.createHash('sha1');
+    sha1.update(arr.join(''));
+    const result = sha1.digest('hex');
+
+    if (result === signature) {
+        ctx.body = params.echostr
+    } else {
+        ctx.body = { code: -1, msg: "fail"}
+    }
+});
