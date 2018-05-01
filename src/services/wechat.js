297b3c7276e2cfbeafaea4795d1510c399d81323
吴俊霖 <wujunlin@wujunlindeMacBook.local>
2018-05-01 10:41:08 +0800

    fix qrcode param

--- src/services/wechat.js
+++ src/services/wechat.js
@@ -261,7 +261,7 @@ exports.qrcode = async(mina_scene, mina_path) => {
     let stream = fs.createWriteStream(ret);
     await superagent.post(api_url).send({
         scene: mina_scene,
-        path: mina_path,
+        page: mina_path,
         width: 60
     }).pipe(stream);
     return filename;
