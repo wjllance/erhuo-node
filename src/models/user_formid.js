51bab16b30460f9da841e9c488004fdef25c2890
吴俊霖 <wujunlin@wujunlindeMacBook.local>
2018-05-16 14:34:26 +0800

    fix formid unique

--- src/models/user_formid.js
+++ src/models/user_formid.js
@@ -7,11 +7,13 @@ let userFormidSchema = new mongoose.Schema({
 
     user_id: {
         type : mongoose.Schema.ObjectId,
-        ref: 'User'
+        ref: 'User',
+        required: true
     },
     formid: {
-        type: String,
-        unique: true
+        type: mongoose.Schema.Types.String,
+        unique: true,
+        required: true
     },
     expire_date: Date
 },{versionKey:false});
