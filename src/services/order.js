afd353902129516966b19b2ad4d6b1fa65ee0514
吴俊霖 <wujunlin@wujunlindeMacBook.local>
2018-06-01 13:48:05 +0800

    update transaction check

--- src/services/order.js
+++ src/services/order.js
@@ -295,7 +295,7 @@ exports.autorefund = async (order) => {
 };
 
 exports.refund_apply = async(order) =>{
-    auth.assert(order.refund_status == REFUND_STATUS.INIT && order.pay_status == PAY_STATUS.SUCCEED, "不可申请退款");
+    auth.assert(order.refund_status === REFUND_STATUS.INIT && order.pay_status === PAY_STATUS.SUCCEED, "不可申请退款");
     auth.assert(!order.finished_date, "订单已结束，申请退款请私下联系或联系客服");
     order.refund_status = REFUND_STATUS.APPLYING;
     await order.save();
@@ -303,7 +303,7 @@ exports.refund_apply = async(order) =>{
 };
 
 exports.refund_confirm = async(order) =>{
-    auth.assert(order.refund_status == REFUND_STATUS.APPLYING, "不可确认退款");
+    auth.assert(order.refund_status === REFUND_STATUS.APPLYING, "不可确认退款");
     order.refund_status = REFUND_STATUS.SUCCEED;
     await order.save();
     //TODO SEND NOTIFY
@@ -312,7 +312,7 @@ exports.refund_confirm = async(order) =>{
 exports.finish = async(orderId) => {
     let order = await Order.findById(orderId);
     auth.assert(order, "订单不存在");
-    auth.assert(order.refund_status == REFUND_STATUS.INIT && order.order_status == ORDER_STATUS.COMPLETE, "不能结束，请检查订单状态");
+    auth.assert(order.refund_status === REFUND_STATUS.INIT && order.order_status === ORDER_STATUS.COMPLETE, "不能结束，请检查订单状态");
     order.finished_date = moment();
     await order.save();
 
