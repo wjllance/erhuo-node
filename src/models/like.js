d603668689049ed421712d295a6e97daafac6e5d
吴俊霖 <wujunlin@wujunlieMacBook.lan>
2018-06-05 13:45:13 +0800

    like init

---
+++ src/models/like.js
@@ -0,0 +1,29 @@
+
+let mongoose = require('mongoose');
+let _ = require('lodash');
+let tools = require('../services/tools')
+
+let log4js = require('log4js');
+let logger = log4js.getLogger('errorLogger');
+
+// 点赞
+let likeSchema = new mongoose.Schema({
+
+    userID: {
+        type:  mongoose.Schema.ObjectId,
+        ref: 'User',
+        require: true
+    },
+    goodsId: {
+        type:  mongoose.Schema.ObjectId,
+        ref: 'Goods',
+        require:true
+    },
+    read_date: Date,
+    canceled_date: Date,
+    updated_date: { type: Date, default: Date.now },
+    created_date: { type: Date, default: Date.now },
+});
+
+
+module.exports = mongoose.model("Like", likeSchema);
