2041eba299c727fbf48e8226ca38ce9aa616fbb5
吴俊霖 <wujunlin@wujunlieMacBook.lan>
2018-06-08 15:07:10 +0800

    test goods auto examin

--- src/schedule.js
+++ src/schedule.js
@@ -17,7 +17,59 @@ function scheduleCronstyle(){
 
 exports.register = function () {
     console.log("register!");
-    if(config.ENV == "local") {
+
+    scheduleOrderCountDown();
+
+    scheduleOrderTimeout();
+
+    scheduleGoodsExamine();
+
+};
+let scheduleGoodsExamine = () =>{
+    schedule.scheduleJob('*/10 * * * * *', async function(){
+        // schedule.scheduleJob('*/5 * * * * *', async function(){
+        console.log('checking goods passed...');
+        let ddl = moment({hour:20});
+        if(moment().isBefore(ddl)){
+            ddl = ddl.subtract(1,'d');
+        }
+        // ddl = moment().subtract(30,'s');
+        let condi = {
+            status:config.CONSTANT.GOODS_STATUS.PASS,
+            created_date: {$lt: ddl}
+        };
+        // console.log(condi);
+        let goods = await Goods.find(condi);
+        // console.log(goods);
+        if(goods.length > 0){
+            console.log("updating...count:",goods.length);
+            await Goods.updateMany(condi,{
+                status: config.CONSTANT.GOODS_STATUS.RELEASED
+            })
+        }
+
+    });
+}
+
+let scheduleOrderTimeout = ()=>{
+    schedule.scheduleJob('0 */1 * * * *', async function() {
+        // schedule.scheduleJob('*/5 * * * * *', async function(){
+        console.log('checking order timeout...');
+        let orders = await Order.find({
+            updated_date: {
+                $lt: moment().subtract(15, 'm')
+            },
+            order_status: config.CONSTANT.ORDER_STATUS.TOPAY
+        });
+        console.log(orders);
+        for (let i = 0; i < orders.length; i++) {
+            await srv_order.cancel(orders[i]);
+        }
+    })
+};
+
+let scheduleOrderCountDown = ()=>{
+    if(config.ENV === "local") {
         schedule.scheduleJob('*/30 * * * * *', async function(){
             console.log('checking normal order countdown...');
             let transactions = await Transaction.find({
@@ -33,6 +85,9 @@ exports.register = function () {
                 await srv_transaction.finish(transactions[i]);
             }
         });
+
+
+
     }else{
         schedule.scheduleJob('0 */10 * * * *', async function(){
             console.log('checking normal order countdown...');
@@ -51,43 +106,4 @@ exports.register = function () {
         });
 
     }
-
-
-    schedule.scheduleJob('0 */1 * * * *', async function(){
-    // schedule.scheduleJob('*/5 * * * * *', async function(){
-        console.log('checking order timeout...');
-        let orders = await Order.find({
-            updated_date: {
-                $lt: moment().subtract(15, 'm')
-            },
-            order_status: config.CONSTANT.ORDER_STATUS.TOPAY
-        });
-        console.log(orders);
-        for(let i = 0; i< orders.length; i++){
-            await srv_order.cancel(orders[i]);
-        }
-
-
-
-
-        console.log('checking goods passed...');
-        let ddl = moment({hour:20});
-        if(moment().isBefore(ddl)){
-            ddl = ddl.subtract(1,'d');
-        }
-        let condi = {
-            status:config.CONSTANT.GOODS_STATUS.PASS,
-            created_date: {$lt: ddl}
-        };
-        let goods = await Goods.find(condi);
-        if(goods.length > 0){
-            console.log("updating...count:",goods.length);
-            await Goods.updateMany(condi,{
-                status: config.CONSTANT.GOODS_STATUS.RELEASED
-            })
-        }
-
-    });
-
-};
-
+}
