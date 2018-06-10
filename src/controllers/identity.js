89b5165f6da6a8c082412206dcfdadd7c2dbec88
吴俊霖 <wujunlin@wujunlindeMacBook.local>
2018-06-10 11:36:18 +0800

    fix identity save

--- src/controllers/identity.js
+++ src/controllers/identity.js
@@ -58,17 +58,8 @@ router.post('/v2/identity/save', auth.loginRequired,async (ctx, next) => {
 
     let identity= new Identity();
     identity.userID=ctx.state.user._id;
-    // let cardimage= await Image.findById(ctx.request.body.cardpics);
-    // let withcardimage= await Image.findById(ctx.request.body.withcardpics);
-
-
-    auth.assert(cardimage, "card image not found");
-    auth.assert(withcardimage, "withcardimage image not found");
 
     _.assign(identity, _.pick(ctx.request.body, ['name','studentID','school', 'ncard', 'nwithcard']));
-    // identity.cardpics = cardimage._id;
-    // identity.withcardpics= withcardimage._id;
-
     await identity.save();
     ctx.body={
         success:1,
