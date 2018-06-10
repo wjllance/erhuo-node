77911f1c1a7294d8dda1e1cd7a9a9c708fd1c523
吴俊霖 <wujunlin@wujunlindeMacBook.local>
2018-06-10 15:54:06 +0800

    test

--- src/schedule.js
+++ src/schedule.js
@@ -60,7 +60,7 @@ let scheduleOldPicsUpload = () =>{
         let goodsall = await Goods.find({"npics.0":{$exists:false}}).populate('gpics');
         console.log("checking old pics upload...");
         console.log(goodsall.length);
-        for (let j = 0; j < 3 && j < goodsall.length; j++) {
+        for (let j = 0; j < 10 && j < goodsall.length; j++) {
             let goods = goodsall[j];
             let npics = [];
             for (let i = 0; i < goods.gpics.length; i++){
@@ -75,7 +75,8 @@ let scheduleOldPicsUpload = () =>{
 
                     npics.push(res.url());
                 }catch (e) {
-                    console.error(e)
+                    console.error(goods._id, e)
+
                 }
 
             }
