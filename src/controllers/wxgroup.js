fddeccf582e694e312b9b4a42cb893e722cad9d1
吴俊霖 <wujunlin@wujunlieMacBook.lan>
2018-07-06 12:02:11 +0800

    test group

--- src/controllers/wxgroup.js
+++ src/controllers/wxgroup.js
@@ -14,18 +14,19 @@ let srv_wxtemplate = require('../services/wechat_template');
 let srv_bargain = require('../services/bargain');
 let srv_wxgroup = require('../services/wxgroup');
 let srv_goods = require('../services/goods');
-let {Order, Goods, Bargain, wxGroup} = require('../models');
+let {Order,User, Goods, Bargain, wxGroup} = require('../models');
+
+let WXBizDataCrypt = require('../services/WXBizDataCrypt');
+
 
 const router = module.exports = new Router();
 
 
 /**
- * @api {get} /bargain/goods/:goods_id  砍价商品详情？
- * @apiName     BargainGoods
- * @apiGroup    Bargain
+ * @api {get}   /group/my       我的群列表
+ * @apiName     MyGroupList
+ * @apiGroup    Group
  *
- *
- * @apiParam    {String}    bargainId
  *
  * @apiSuccess  {Number}    success
  * @apiSuccess  {Object}    data
@@ -33,6 +34,12 @@ const router = module.exports = new Router();
  */
 router.get('/group/my', auth.loginRequired, async (ctx, next) => {
 
+    let mygroup = await wxGroup.find({userID:ctx.state.user._id});
+
+    ctx.body = {
+        success:1,
+        data: mygroup
+    }
 });
 
 
@@ -41,6 +48,16 @@ router.del('/group/:openGId', auth.loginRequired, async (ctx, next)=>{
 });
 
 
+/**
+ * @api {get}   /group/:groupId/feed    群商品
+ * @apiName     GroupFeed
+ * @apiGroup    Group
+ *
+ *
+ * @apiSuccess  {Number}    success
+ * @apiSuccess  {Object}    data
+ *
+ */
 router.get('/group/:groupId/feed', async (ctx, next) => {
 
     let group = await wxGroup.findById(ctx.params.groupId);
@@ -103,14 +120,34 @@ router.get('/group/:groupId/feed', async (ctx, next) => {
     }
 });
 
-router.post('/group/join', auth.loginRequired, async (ctx, next) => {
 
-    let params = ctx.request.body;
-    let condi = {openGId: params.openGId};
-    let data = {name: params.groupName};
-    let wxgroup = await wxGroup.findOneAndUpdate(condi, data, {new:true, upsert:true});
+/**
+ * @api {post}  /group/:groupId/join    加入群
+ * @apiName     GroupJoin
+ * @apiGroup    Group
+ *
+ *
+ * @apiParam    {String}    groupId
+ * @apiParam    {String}    [userId]
+ * @apiParam    {Boolean}   [GOD]
+ *
+ *
+ * @apiSuccess  {Number}    success
+ * @apiSuccess  {Object}    data
+ *
+ */
+router.post('/group/join', async (ctx, next) => {
 
-    await srv_wxgroup.createUserGroup(wxgroup, ctx.state.user);
+    let groupId = ctx.request.body.groupId;
+
+    let wxgroup = await wxGroup.findById(groupId);
+    auth.assert(wxgroup, "群不在");
+    let user = ctx.state.user;
+    if (ctx.request.body.GOD){
+        user = await User.findById(ctx.request.body.userId);
+    }
+    auth.assert(user, "没有人");
+    await srv_wxgroup.createUserGroup(wxgroup, user);
 
     ctx.body = {
         success:1,
@@ -118,8 +155,52 @@ router.post('/group/join', auth.loginRequired, async (ctx, next) => {
     }
 });
 
+/**
+ * @api {post}  /group/update    群信息更新
+ * @apiName     GroupUpdate
+ * @apiGroup    Group
+ *
+ * @apiParam    {String}    encryptedData       加密信息
+ * @apiParam    {String}    iv
+ *
+ * @apiParam    {String}    [groupName]   群名
+ *
+ * @apiSuccess  {Number}    success
+ * @apiSuccess  {Object}    data
+ *
+ */
+router.post('/group/update', auth.loginRequired, async (ctx, next) =>{
+
+    let reqParams = ctx.request.body;
+    let pc = new WXBizDataCrypt(config.APP_ID, ctx.state.user.session_key);
+    let decryptedData = pc.decryptData(reqParams.m, reqParams.iv);
+
+    console.log(decryptedData);
+    auth.assert(decryptedData.openGId, "没");
+
+    let data = {};
+    if(reqParams.groupName){
+        data.name = reqParams.groupName;
+    }
+    let wxgroup = await wxGroup.findOneAndUpdate({
+        openGId:decryptedData.openGId
+    }, data, {new:true, upsert:true});
+    ctx.body = {
+        success:1,
+        data: wxgroup._id
+    }
+});
 
 
+/**
+ * @api {get}  /group/:groupId/members    全部群成员
+ * @apiName     GroupMembers
+ * @apiGroup    Group
+ *
+ * @apiSuccess  {Number}    success
+ * @apiSuccess  {Object}    data
+ *
+ */
 router.get('/group/:groupId/members', auth.loginRequired, async (ctx, next) => {
 
     let ret = await srv_wxgroup.getMembers(ctx.params.groupId);
