bbd92062bbc1a63a99be019e6c740ad60b8c809f
Junlin Wu <wjllance@163.com>
2018-07-29 14:22:35 +0800

    school list

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,11 +2,29 @@
 require('should');
 let Router = require('koa-router');
 
+let config = require('../config');
+let _ = require('lodash');
 const router = module.exports = new Router();
 
 router.get('/ping', async ctx => {
     ctx.body = {
         data: 'pong'
+    }
+});
+
+
+router.get('/schools', async(ctx, next) =>{
+    let schools = config.CONSTANT.SCHOOL_MAP;
+    let ret = _.map(schools, (p) =>{
+        return {
+            name: p,
+            show: false
+        }
+    });
+    ret.splice(0,2);
+    ctx.body = {
+        success:1,
+        data:ret
     }
 });
 
