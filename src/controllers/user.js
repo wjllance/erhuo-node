8ba9e72f2d5be2b9184f8658a7fcab3560971970
Junlin Wu <wjllance@163.com>
2018-10-14 09:25:17 +0800

    fix user locationName

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -51,20 +51,29 @@ router.post('/user/update', auth.loginRequired, async (ctx, next) => {
 
     console.log("here is update");
 
-    auth.assert(ctx.request.body.signature === utils.sha1(ctx.request.body.rawData + ctx.state.user.session_key), '签名错误1');
+    let loginUser = ctx.state.user;
+    auth.assert(ctx.request.body.signature === utils.sha1(ctx.request.body.rawData + loginUser.session_key), '签名错误1');
 
-    let pc = new WXBizDataCrypt(config.APP_ID, ctx.state.user.session_key);
+    let pc = new WXBizDataCrypt(config.APP_ID, loginUser.session_key);
     let data = pc.decryptData(ctx.request.body.encryptedData, ctx.request.body.iv);
 
     auth.assert(data.openId === ctx.state.user.openid, '签名错误2');
     auth.assert(data.watermark.appid === config.APP_ID, '水印错误');
     console.log(data);
-    _.assign(ctx.state.user, _.pick(ctx.request.body.userInfo, ['nickName', 'unionid', 'avatarUrl', 'gender', 'city', 'province', 'country', 'language']));
-    ctx.state.user.unionid = data.unionId;
-    ctx.state.user.updated_date = moment();
-    let user = await ctx.state.user.save();
+    _.assign(loginUser, _.pick(ctx.request.body.userInfo, ['nickName', 'unionid', 'avatarUrl', 'gender', 'city', 'province', 'country', 'language']));
+    loginUser.unionid = data.unionId;
+    loginUser.updated_date = moment();
 
-    let userIndex = await srv_user.indexInfo(user._id);
+
+    if(loginUser.location && !loginUser.locationName){
+        let univ = await University.findOne({
+            locationNum: loginUser.location
+        });
+        loginUser.locationName = univ.name;
+    }
+    ctx.state.user = await loginUser.save();
+
+    let userIndex = await srv_user.indexInfo(loginUser._id);
     ctx.body = {
         success: 1,
         data: userIndex
