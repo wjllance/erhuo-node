2a3569aca7e5d5942324ed134fa9a7ee9f113bc9
Sulliven <13893461064@163.com>
2018-10-15 13:54:30 +0800

    auth

--- src/controllers/identity.js
+++ src/controllers/identity.js
@@ -7,6 +7,7 @@ let path = require("path");
 let body = require("koa-convert")(require("koa-better-body")());
 let config = require("../config");
 let auth = require("../services/auth");
+let identityService = require('../services/identity');
 
 
 let { User, Image, Identity } = require("../models");
@@ -48,12 +49,46 @@ router.post("/v2/identity/save", auth.loginRequired, async (ctx, next) => {
         data: identity._id,
     };
 });
-
-router.get("/identity/info", auth.loginRequired, async (ctx, next) => {
-    let identity = await Identity.findOne({ userID: ctx.state.user._id }).sort({ created_date: -1 });
+/**
+ * @api {get}    /identity/userInfoDetail  获取单个用户的认证详情
+ * @apiName     userList
+ * @apiGroup    identity
+ *
+ *
+ *
+ * @apiSuccess  {Number}    success     1success
+ * @apiSuccess  {Object}    data        用户的详情信息
+ *
+ */
+router.get("/identity/userInfoDetail", auth.loginRequired, async (ctx, next) => {
+    let identity = await Identity.findOne({ userID: ctx.state.user._id });
     let ret = identity || null;
     ctx.body = {
         success: 1,
         data: ret,
+    };
+});
+/**
+ * @api {get}    /identity/userlist  上获取认证列表
+ * @apiName     userList
+ * @apiGroup    identity
+ *
+ *
+ *
+ * @apiSuccess  {Number}    success     1success
+ * @apiSuccess  {Object}    data        分页认证列表
+ *
+ */
+router.get("/identity/userlist", auth.loginRequired, async (ctx, next) => {
+
+
+    let pageNo = ctx.query.pageNo || 1;
+    let pageSize = Math.min(ctx.query.pageSize || 6, 20); // 最大20，默认6
+
+    let identitys = await identityService.userList(pageNo,pageSize);
+     console.log(identitys);
+    ctx.body = {
+        success: 1,
+        data: identitys,
     };
 });
---
+++ src/services/identity.js
@@ -0,0 +1,24 @@
+let _ = require("lodash");
+let Identity = require('../models/identity');
+exports.userList = async (pageNo, pageSize) => {
+
+    let sorti = {
+        created_date: -1,
+    };
+    let total = await Identity.find().count();//表总记录数
+
+    let identitys= await Identity.find()
+        .sort(sorti)
+        .limit(pageSize)
+        .skip((pageNo - 1) * pageSize)
+    let hasMore = total - pageNo * pageSize > 0;
+    let identity =[];
+    for(var i=0;i<identitys.length;i++){
+        identity[i]= _.pick(identitys[i], ["name", "studentID", "school", "ncard", "nwithcard","status"]);
+    }
+    return {
+        users: identity,
+        hasMore: hasMore,
+        total: total,
+    };
+};
